c55b1ca0c29a83837d2284993787cb4711b9119a
Artemii29 <artemii.kharitonov@bk.ru>
2023-10-03 21:32:13 +0300

    first task

---
+++ src/task1.java
@@ -0,0 +1,28 @@
+public class task1 {
+    public static void main(String[] args){
+    //task1
+        int allMessage = 72;
+        int sendMessage = 45;
+        int remains = allMessage - sendMessage;
+        System.out.println(remains);
+        //task2
+        float time = 4;
+        float distance = 6;
+        float speed = (distance * 1000)/(time*3600);
+        System.out.println(speed);
+
+        //task3
+    int sum = 34;
+    int sumTwo = sum%10 + sum/10;
+        System.out.println(sumTwo);
+    //task4
+        int x = 10;
+        int y = 7;
+        int a = y;
+        y = x;
+        x = a;
+        System.out.println(y);
+        System.out.println(x);
+
+    }
+    }
